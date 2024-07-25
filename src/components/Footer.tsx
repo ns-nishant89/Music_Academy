4596ce3e6cb899d6a9d8e85ad08228cbec208794
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-07-25 08:53:14 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 
-function Footer() {
+function Footer() { 
   return (
     <footer className="bg-black text-gray-400 py-12">
         <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
