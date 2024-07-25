8014e4e2d7bd69d6b2000eedd26eb1049c9e4c2f
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2024-07-25 08:57:18 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border";
 
-function HeroSection() {
+function HeroSection() { 
   return (
     <div
     className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
